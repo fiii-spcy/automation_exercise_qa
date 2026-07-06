24537ab233748dd5610b5f512253c793bbc0ae7e
Muhammad Hashfi <hashfi1212@gmail.com>
2026-07-06 13:41:01 +0700

    Fix Chrome headless config for GitHub Actions

--- conftest.py
+++ conftest.py
@@ -1,20 +1,26 @@
 ﻿import pytest
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
-from webdriver_manager.chrome import ChromeDriverManager
 
 
 @pytest.fixture
 def driver():
     options = Options()
-    options.add_argument("--start-maximized")
-    options.add_argument("--disable-notifications")
 
-    driver = webdriver.Chrome(
-        service=Service(ChromeDriverManager().install()),
-        options=options
-    )
+    # Wajib untuk GitHub Actions / Linux CI
+    options.add_argument("--headless=new")
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
+    options.add_argument("--disable-gpu")
+    options.add_argument("--window-size=1920,1080")
+
+    # Tambahan umum
+    options.add_argument("--disable-notifications")
+    options.add_argument("--disable-extensions")
+
+    driver = webdriver.Chrome(options=options)
+
+    driver.implicitly_wait(10)
 
     yield driver
 
